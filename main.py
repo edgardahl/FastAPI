066500ba22428f8ae0bf7ce11b9e61421f80435c
Edgar Dahl <edgardahl123@gmail.com>
2024-02-21 12:28:53 +0100

    test

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!", }
+
+@app.post("/")
+async def root():
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!", "test": "This is a test!"}
